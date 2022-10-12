3372ff62d5161375e1330b074bf58657eaacb7ad
Ionuț Ciuta <ionutciuta24@gmail.com>
2022-10-12 23:02:27 +0300

    Add top-down DP (memoization)

--- src/main/java/com/ionutciuta/dynamicprogramming/problem03/MinCostPathCalculator.java
+++ src/main/java/com/ionutciuta/dynamicprogramming/problem03/MinCostPathCalculator.java
@@ -2,6 +2,9 @@ package com.ionutciuta.dynamicprogramming.problem03;
 
 import com.ionutciuta.dynamicprogramming.tools.IntMatrix;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MinCostPathCalculator {
   private MinCostPathCalculator() {}
 
@@ -15,16 +18,55 @@ public class MinCostPathCalculator {
     }
 
     if (i == matrix.getRowCount() - 1) {
-      return  matrix.getValue(i, j) + computeRecursively(i, j + 1, matrix);
+      return matrix.getValue(i, j) + computeRecursively(i, j + 1, matrix);
     }
 
     if (j == matrix.getColumnCount() - 1) {
       return matrix.getValue(i, j) + computeRecursively(i + 1, j, matrix);
     }
 
-    return matrix.getValue(i, j) + Math.min(
-            computeRecursively(i + 1, j, matrix),
-            computeRecursively(i, j + 1, matrix)
-    );
+    return matrix.getValue(i, j)
+        + Math.min(computeRecursively(i + 1, j, matrix), computeRecursively(i, j + 1, matrix));
+  }
+
+  // DP top-down
+  public static int computeRecursivelyWithMemo(IntMatrix matrix) {
+    final var cache = new HashMap<String, Integer>();
+    return computeRecursivelyWithMemo(0, 0, matrix, cache);
+  }
+
+  private static int computeRecursivelyWithMemo(int i, int j, IntMatrix matrix, Map<String, Integer> cache) {
+    final var key = keyOf(i, j);
+    if (cache.containsKey(key)) {
+      return cache.get(key);
+    }
+
+    if (i == matrix.getRowCount() - 1 && j == matrix.getColumnCount() - 1) {
+      return matrix.getValue(i, j);
+    }
+
+    if (i == matrix.getRowCount() - 1) {
+      final var cost = matrix.getValue(i, j) + computeRecursivelyWithMemo(i, j + 1, matrix, cache);
+      cache.put(key, cost);
+      return cost;
+    }
+
+    if (j == matrix.getColumnCount() - 1) {
+      final var cost = matrix.getValue(i, j) + computeRecursivelyWithMemo(i + 1, j, matrix, cache);
+      cache.put(key, cost);
+      return cost;
+    }
+
+    final var cost =
+        matrix.getValue(i, j)
+            + Math.min(
+                computeRecursivelyWithMemo(i + 1, j, matrix, cache),
+                computeRecursivelyWithMemo(i, j + 1, matrix, cache));
+    cache.put(key, cost);
+    return cost;
+  }
+
+  private static String keyOf(int x, int y) {
+    return x + ":" + y;
   }
 }
--- src/test/java/com/ionutciuta/dynamicprogramming/problem03/MinCostPathCalculatorTest.java
+++ src/test/java/com/ionutciuta/dynamicprogramming/problem03/MinCostPathCalculatorTest.java
@@ -1,6 +1,7 @@
 package com.ionutciuta.dynamicprogramming.problem03;
 
 import com.ionutciuta.dynamicprogramming.tools.IntMatrix;
+import com.ionutciuta.dynamicprogramming.tools.MatrixGenerator;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -8,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class MinCostPathCalculatorTest {
 
   @Test
-  public void shouldReturnMinCostRecursively1() {
+  public void shouldReturnMinCostPath() {
     var m =
         new IntMatrix(
             new Integer[][] {
@@ -17,31 +18,51 @@ class MinCostPathCalculatorTest {
             });
 
     assertEquals(4, MinCostPathCalculator.computeRecursively(m));
+    assertEquals(4, MinCostPathCalculator.computeRecursivelyWithMemo(m));
   }
 
-    @Test
-    public void shouldReturnMinCostRecursively2() {
-        var m =
-                new IntMatrix(
-                        new Integer[][] {
-                                new Integer[] {1, 2},
-                                new Integer[] {5, 2}
-                        });
+  @Test
+  public void shouldReturnMinCostPath2() {
+    var m =
+        new IntMatrix(
+            new Integer[][] {
+              new Integer[] {1, 2},
+              new Integer[] {5, 2}
+            });
 
-        assertEquals(5, MinCostPathCalculator.computeRecursively(m));
-    }
+    assertEquals(5, MinCostPathCalculator.computeRecursively(m));
+    assertEquals(5, MinCostPathCalculator.computeRecursivelyWithMemo(m));
+  }
 
-    @Test
-    public void shouldReturnMinCostRecursively3() {
-        var m =
-                new IntMatrix(
-                        new Integer[][] {
-                                new Integer[] { 3,  2, 12, 15, 10 },
-                                new Integer[] { 6, 19,  7, 11, 17 },
-                                new Integer[] { 8,  5, 12, 32, 21 },
-                                new Integer[] { 3, 20,  2,  9,  7 }
-                        });
+  @Test
+  public void shouldReturnMinCostPath3() {
+    var m =
+        new IntMatrix(
+            new Integer[][] {
+              new Integer[] {3, 2, 12, 15, 10},
+              new Integer[] {6, 19, 7, 11, 17},
+              new Integer[] {8, 5, 12, 32, 21},
+              new Integer[] {3, 20, 2, 9, 7}
+            });
 
-        assertEquals(52, MinCostPathCalculator.computeRecursively(m));
-    }
+    assertEquals(52, MinCostPathCalculator.computeRecursively(m));
+    assertEquals(52, MinCostPathCalculator.computeRecursivelyWithMemo(m));
+  }
+
+  @Test
+  public void timeSolutions() {
+    final var m = (IntMatrix) MatrixGenerator.getRandomIntMatrix(17, 10);
+
+    long start = System.currentTimeMillis();
+    final var costRecursively = MinCostPathCalculator.computeRecursively(m);
+    long duration = System.currentTimeMillis() - start;
+    System.out.println("computeRecursively: " + duration + " ms");
+
+    start = System.currentTimeMillis();
+    final var costRecursivelyWithMemo = MinCostPathCalculator.computeRecursivelyWithMemo(m);
+    duration = System.currentTimeMillis() - start;
+    System.out.println("computeRecursivelyWithMemo: " + duration + " ms");
+
+    assertEquals(costRecursively, costRecursivelyWithMemo);
+  }
 }
