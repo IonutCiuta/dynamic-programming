5ce3ac9956fd02c1a1a773a9e0044c92f85e50cb
Ionuț Ciuta <ionutciuta24@gmail.com>
2022-11-05 21:13:46 +0200

    Recursive & memo

--- src/main/java/com/ionutciuta/dynamicprogramming/problem03/LongestSubsequence.java
+++ src/main/java/com/ionutciuta/dynamicprogramming/problem03/LongestSubsequence.java
@@ -1,20 +1,67 @@
 package com.ionutciuta.dynamicprogramming.problem03;
 
-import com.ionutciuta.dynamicprogramming.problem02.HouseRobber;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LongestSubsequence {
-    private static LongestSubsequence instance;
+  private static LongestSubsequence instance;
 
-    private LongestSubsequence() {}
+  private LongestSubsequence() {}
 
-    public static synchronized LongestSubsequence getInstance() {
-        if (instance == null) {
-            instance = new LongestSubsequence();
-        }
-        return instance;
+  public static synchronized LongestSubsequence getInstance() {
+    if (instance == null) {
+      instance = new LongestSubsequence();
+    }
+    return instance;
+  }
+
+  public int computeRecursively(char[] s1, char[] s2) {
+    return computeRecursively(0, 0, s1, s2);
+  }
+
+  public int computeRecursively(int i, int j, char[] s1, char[] s2) {
+    if (i >= s1.length || j >= s2.length) {
+      return 0;
     }
 
-    public int computeRecursively(char[] s1, char[] s2) {
-        return 0;
+    if (s1[i] == s2[j]) {
+      return 1 + computeRecursively(i + 1, j + 1, s1, s2);
+    } else {
+      return Math.max(computeRecursively(i, j + 1, s1, s2), computeRecursively(i + 1, j, s1, s2));
     }
+  }
+
+  public int computeRecursivelyWithMemo(char[] s1, char[] s2) {
+    return computeRecursivelyWithMemo(0, 0, s1, s2, new HashMap<>());
+  }
+
+  private int computeRecursivelyWithMemo(
+      int i, int j, char[] s1, char[] s2, Map<String, Integer> cache) {
+    if (i >= s1.length || j >= s2.length) {
+      return 0;
+    }
+
+    final var key = keyOf(i, j);
+
+    if (cache.containsKey(key)) {
+      return cache.get(key);
+    }
+
+    int result;
+    if (s1[i] == s2[j]) {
+      result = 1 + computeRecursivelyWithMemo(i + 1, j + 1, s1, s2, cache);
+    } else {
+      result =
+          Math.max(
+              computeRecursivelyWithMemo(i, j + 1, s1, s2, cache),
+              computeRecursivelyWithMemo(i + 1, j, s1, s2, cache));
+    }
+
+    cache.put(key, result);
+    return result;
+  }
+
+  private String keyOf(int i, int j) {
+    return i + ":" + j;
+  }
 }
--- src/test/java/com/ionutciuta/dynamicprogramming/problem03/LongestSubsequenceTest.java
+++ src/test/java/com/ionutciuta/dynamicprogramming/problem03/LongestSubsequenceTest.java
@@ -1,7 +1,38 @@
 package com.ionutciuta.dynamicprogramming.problem03;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LongestSubsequenceTest {
+  private final LongestSubsequence calculator = LongestSubsequence.getInstance();
 
+  @ParameterizedTest
+  @MethodSource("input")
+  public void shouldReturnLcs_recursive(String s1, String s2, int expected) {
+    assertEquals(expected, calculator.computeRecursively(s1.toCharArray(), s2.toCharArray()));
+  }
+
+  @ParameterizedTest
+  @MethodSource("input")
+  public void shouldReturnLcs_recursiveWithMemo(String s1, String s2, int expected) {
+    assertEquals(
+        expected, calculator.computeRecursivelyWithMemo(s1.toCharArray(), s2.toCharArray()));
+  }
+
+  public static Stream<Arguments> input() {
+    return Stream.of(
+        Arguments.of("a", "b", 0),
+        Arguments.of("a", "a", 1),
+        Arguments.of("a", "ba", 1),
+        Arguments.of("a", "ab", 1),
+        Arguments.of("ab", "ab", 2),
+        Arguments.of("ab", "acb", 2),
+        Arguments.of("acb", "defgacb", 3),
+        Arguments.of("a0c0b", "adefg1c2b3", 3));
+  }
 }
