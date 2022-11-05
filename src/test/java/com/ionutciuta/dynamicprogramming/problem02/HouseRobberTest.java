a69650cb99b84b126e4dcf1cfd2af91533f06e4b
Ionuț Ciuta <ionutciuta24@gmail.com>
2022-11-05 18:11:50 +0200

    Add course solutions

--- src/test/java/com/ionutciuta/dynamicprogramming/problem02/HouseRobberTest.java
+++ src/test/java/com/ionutciuta/dynamicprogramming/problem02/HouseRobberTest.java
@@ -17,6 +17,30 @@ class HouseRobberTest {
     assertEquals(expected, calculator.computeRecursively(houses));
   }
 
+  @ParameterizedTest
+  @MethodSource("input")
+  public void shouldReturnMostMoneyThatCanBeStolen_recursiveCourseSolution(int[] houses, int expected) {
+    assertEquals(expected, calculator.computeRecursively(houses));
+  }
+
+  @ParameterizedTest
+  @MethodSource("input")
+  public void shouldReturnMostMoneyThatCanBeStolen_recursiveCourseSolutionWithMemo(int[] houses, int expected) {
+    assertEquals(expected, calculator.recursiveSolutionWithMemo(houses));
+  }
+
+  @ParameterizedTest
+  @MethodSource("input")
+  public void shouldReturnMostMoneyThatCanBeStolen_dpSolution(int[] houses, int expected) {
+    assertEquals(expected, calculator.dpSolution(houses));
+  }
+
+  @ParameterizedTest
+  @MethodSource("input")
+  public void shouldReturnMostMoneyThatCanBeStolen_dpSolutionWithSpaceOptimization(int[] houses, int expected) {
+    assertEquals(expected, calculator.dpSolutionWithSpaceOptimization(houses));
+  }
+
   public static Stream<Arguments> input() {
     return Stream.of(
         Arguments.of(new int[] {}, 0),
@@ -25,6 +49,7 @@ class HouseRobberTest {
         Arguments.of(new int[] {1, 10, 3}, 10),
         Arguments.of(new int[] {1, 10, 10}, 11),
         Arguments.of(new int[] {1, 10, 9, 2}, 12),
-        Arguments.of(new int[] {2, 10, 3, 6, 8, 1, 7}, 25));
+        Arguments.of(new int[] {2, 10, 3, 6, 8, 1, 7}, 25),
+        Arguments.of(new int[] {4, 8, 12, 1, 2, 10, 3, 6, 8}, 34));
   }
 }
